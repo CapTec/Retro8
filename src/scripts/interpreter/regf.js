56ec9fbc9a65d5f02d87433fffb0da7b76dabd9f
Daniel Lane <daniel@application-developer.org>
2017-08-18 13:21:34 +0100

    Implemented FX65

--- src/scripts/interpreter/regf.js
+++ src/scripts/interpreter/regf.js
@@ -167,8 +167,12 @@ define(['./errors/notimplemented'], function(CodeNotImplemented) {
    * @param {UInt8} opcode - 8 bit opcode value
    */
   function regLoad(opcode) {
+    var vx = (opcode & 0x0F00) >> 8;
+    for (var i = 0; i <= vx; i++) {
+      this.registers[i] = this.memory[this.index_register + i];
+    }
+
     this.program_counter += 2;
-    throw new CodeNotImplemented(opcode);
   }
 
   return {
